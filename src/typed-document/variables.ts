2d33068010d9a078d9cdb9efc2aa7d5b0d78bcc2
Jovi De Croock <decroockjovi@gmail.com>
2023-11-17 12:10:09 +0100

    cleanup variable derivaiton

--- src/typed-document/variables.ts
+++ src/typed-document/variables.ts
@@ -1,62 +1,78 @@
 import type { Kind, TypeNode } from '@0no-co/graphql.web';
-import type { schema } from '../__tests__/introspection.test-d';
-import type { Introspection } from '../introspection';
+import type { Introspection as IntrospectionType } from '../introspection';
 
-// TODO: input objects
-type UnwrapType<Type extends TypeNode, I extends Introspection<any>> = Type extends {
-  kind: Kind.LIST_TYPE;
-  type: any;
-}
-  ? Array<UnwrapType<Type['type'], I>> | null
-  : Type extends { kind: Kind.NON_NULL_TYPE; type: any }
-  ? NonNullable<UnwrapType<Type['type'], I>>
-  : Type extends { kind: 'NamedType'; name: any }
-  ? Type['name'] extends { kind: Kind.NAME; value: string }
-    ? Type['name']['value'] extends keyof I['types']
-      ? I['types'][Type['name']['value']] extends { kind: 'SCALAR'; type: any }
-        ? I['types'][Type['name']['value']]['type'] extends string
-          ? string | null
-          : I['types'][Type['name']['value']]['type'] extends boolean
-          ? boolean | null
-          : I['types'][Type['name']['value']]['type'] extends number
-          ? number | null
-          : I['types'][Type['name']['value']]['type'] extends string | number
-          ? string | number | null
-          : I['types'][Type['name']['value']]['type'] extends bigint
-          ? bigint | null
-          : never
+type ScalarType<
+  Type extends { kind: 'NamedType'; name: any },
+  Introspection extends IntrospectionType<any>
+> = Type['name'] extends { kind: Kind.NAME; value: string }
+  ? Type['name']['value'] extends keyof Introspection['types']
+    ? Introspection['types'][Type['name']['value']] extends { kind: 'SCALAR'; type: any }
+      ? Introspection['types'][Type['name']['value']]['type'] extends string
+        ? string | null
+        : Introspection['types'][Type['name']['value']]['type'] extends boolean
+        ? boolean | null
+        : Introspection['types'][Type['name']['value']]['type'] extends number
+        ? number | null
+        : Introspection['types'][Type['name']['value']]['type'] extends string | number
+        ? string | number | null
+        : Introspection['types'][Type['name']['value']]['type'] extends bigint
+        ? bigint | null
         : never
       : never
     : never
   : never;
 
-type VariablesContinue<Variables extends readonly any[], I extends Introspection<typeof schema>> = {
-  [Name in Variables[0]['variable']['name']['value']]: UnwrapType<Variables[0]['type'], I>;
+// TODO: input objects
+type UnwrapType<
+  Type extends TypeNode,
+  Introspection extends IntrospectionType<any>
+> = Type extends {
+  kind: Kind.LIST_TYPE;
+  type: any;
+}
+  ? Array<UnwrapType<Type['type'], Introspection>> | null
+  : Type extends { kind: Kind.NON_NULL_TYPE; type: any }
+  ? NonNullable<UnwrapType<Type['type'], Introspection>>
+  : Type extends { kind: 'NamedType'; name: any }
+  ? ScalarType<Type, Introspection>
+  : never;
+
+type VariablesContinue<
+  Variables extends readonly any[],
+  Introspection extends IntrospectionType<any>
+> = {
+  [Name in Variables[0]['variable']['name']['value']]: UnwrapType<
+    Variables[0]['type'],
+    Introspection
+  >;
 } & (Variables extends readonly []
-  ? {}
+  ? never
   : Variables extends readonly [any, ...infer Rest]
   ? Rest extends readonly []
-    ? {}
-    : VariablesContinue<Rest, I>
-  : {});
+    ? never
+    : VariablesContinue<Rest, Introspection>
+  : never);
 
-type DefinitionContinue<T extends any[], I extends Introspection<typeof schema>> = (T[0] extends {
+type DefinitionContinue<
+  Definitions extends any[],
+  Introspection extends IntrospectionType<any>
+> = (Definitions[0] extends {
   kind: Kind.OPERATION_DEFINITION;
   variableDefinitions: any[];
 }
-  ? T[0]['variableDefinitions'] extends Array<{ kind: Kind.VARIABLE_DEFINITION }>
-    ? VariablesContinue<T[0]['variableDefinitions'], I>
-    : { novar: true }
-  : { noOperation: true }) &
-  (T extends readonly []
-    ? {}
-    : T extends readonly [any, ...infer Rest]
+  ? Definitions[0]['variableDefinitions'] extends Array<{ kind: Kind.VARIABLE_DEFINITION }>
+    ? VariablesContinue<Definitions[0]['variableDefinitions'], Introspection>
+    : never
+  : never) &
+  (Definitions extends readonly []
+    ? never
+    : Definitions extends readonly [any, ...infer Rest]
     ? Rest extends readonly []
-      ? {}
-      : DefinitionContinue<Rest, I>
-    : {});
+      ? never
+      : DefinitionContinue<Rest, Introspection>
+    : never);
 
 export type Variables<
   D extends { kind: Kind.DOCUMENT; definitions: any[] },
-  I extends Introspection<typeof schema>
+  I extends IntrospectionType<any>
 > = DefinitionContinue<D['definitions'], I>;
