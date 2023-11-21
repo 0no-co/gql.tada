7addeb347e2cd0d280b52c8d8f1698bc6d4f4c72
Phil Pluckthun <phil@kitten.sh>
2023-11-21 10:58:02 +0000

    Replace lint-staged precommit script

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,18 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const hookSource = path.resolve(__dirname, '../node_modules/husky-v4/sh/husky.sh');
-const hook = path.resolve(__dirname, '../.git/hooks/husky.sh');
-const localHook = path.resolve(__dirname, '../.git/hooks/husky.local.sh');
-const gitConfig = path.resolve(__dirname, '../.git/config');
+const precommit = path.resolve(__dirname, '../.git/hooks/pre-commit');
 
-let script = fs.readFileSync(hookSource, { encoding: 'utf-8' });
-script = script.replace(`$(basename "$0")`, `$(basename "$0" .sh)`);
+const hook = `
+#!/bin/sh
+pnpm exec lint-staged --quiet --relative
+`.trim();
 
-let config = fs.readFileSync(gitConfig, { encoding: 'utf-8' });
-config = config.replace(/\s*hooksPath\s*=\s*\.husky\n?/g, '\n');
-
-fs.writeFileSync(hook, script);
-fs.writeFileSync(gitConfig, config);
-
-fs.writeFileSync(localHook, 'packageManager=yarn\n' + 'cd "."\n');
+fs.writeFileSync(precommit, hook);
+fs.chmodSync(precommit, '755');
