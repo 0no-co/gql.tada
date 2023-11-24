44e0e11d11fe75a8123c9c8dbabfeff8dc03c795
Phil Pluckthun <phil@kitten.sh>
2023-11-24 09:53:22 +0000

    Separate TypedDocument from Document benchmarks

---
+++ src/__tests__/document.bench.ts
@@ -0,0 +1,35 @@
+import { describe, bench } from 'vitest';
+import * as ts from './tsHarness';
+
+describe('TypedDocument', () => {
+  const virtualHost = ts.createVirtualHost({
+    ...ts.readVirtualModule('@0no-co/graphql.web'),
+    'introspection.ts': ts.readFileFromRoot('src/kitchen-sink/index.ts'),
+    'parser.ts': ts.readFileFromRoot('src/parser.ts'),
+    'typed-document.ts': ts.readFileFromRoot('src/typed-document/index.ts'),
+    'variables.ts': ts.readFileFromRoot('src/typed-document/variables.ts'),
+    'index.ts': `
+      import type { GitHubIntrospection } from './introspection';
+      import type { Document } from './parser';
+      import type { TypedDocument } from './typed-document';
+      import type { Variables } from './variables';
+
+      type document  = Document<'
+        query ($org: String!, $repo: String!) {
+          repository(owner: $org, name: $repo) {
+            id
+          }
+        }
+      '>;
+
+      type Result = TypedDocument<document, GitHubIntrospection>
+      type Input = Variables<document, GitHubIntrospection>
+    `,
+  });
+
+  const typeHost = ts.createTypeHost(['index.ts'], virtualHost);
+
+  bench('derives typed document', () => {
+    ts.createTypeChecker(typeHost).getDiagnostics();
+  });
+});
