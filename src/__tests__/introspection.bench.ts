3ec6b187dcc1bd71ef247cf85aef38227afb0b5d
Phil Pluckthun <phil@kitten.sh>
2023-12-01 12:51:03 +0000

    Add introspection benchmarks

---
+++ src/__tests__/introspection.bench.ts
@@ -0,0 +1,54 @@
+import { describe, bench } from 'vitest';
+import * as ts from './tsHarness';
+
+describe('Introspection', () => {
+  (function () {
+    const virtualHost = ts.createVirtualHost({
+      ...ts.readVirtualModule('@0no-co/graphql.web'),
+      ...ts.readSourceFolders(['.']),
+      'simpleIntrospection.ts': ts.readFileFromRoot(
+        'src/__tests__/fixtures/simpleIntrospection.ts'
+      ),
+      'index.ts': `
+        import { simpleIntrospection } from './simpleIntrospection';
+        import type { Introspection } from './introspection';
+
+        type Schema = Introspection<typeof simpleIntrospection>;
+      `,
+    });
+
+    const typeHost = ts.createTypeHost({
+      rootNames: ['index.ts'],
+      host: virtualHost,
+    });
+
+    bench('converts simple introspection', () => {
+      ts.runDiagnostics(typeHost);
+    });
+  })();
+
+  (function () {
+    const virtualHost = ts.createVirtualHost({
+      ...ts.readVirtualModule('@0no-co/graphql.web'),
+      ...ts.readSourceFolders(['.']),
+      'githubIntrospection.ts': ts.readFileFromRoot(
+        'src/__tests__/fixtures/githubIntrospection.ts'
+      ),
+      'index.ts': `
+        import { githubIntrospection } from './githubIntrospection';
+        import type { Introspection } from './introspection';
+
+        type Schema = Introspection<typeof githubIntrospection>;
+      `,
+    });
+
+    const typeHost = ts.createTypeHost({
+      rootNames: ['index.ts'],
+      host: virtualHost,
+    });
+
+    bench('converts github introspection', () => {
+      ts.runDiagnostics(typeHost);
+    });
+  })();
+});
