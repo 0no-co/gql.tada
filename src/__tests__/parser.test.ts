f8b7114670f3d99f6b0f78fee64e7d63988305da
Phil Pluckthun <phil@kitten.sh>
2023-12-01 10:51:11 +0000

    Add unit test for parser

---
+++ src/__tests__/parser.test.ts
@@ -0,0 +1,33 @@
+import { test } from 'vitest';
+import * as ts from './tsHarness';
+
+const testTypeHost = test.each([
+  { strictNullChecks: false, noImplicitAny: false },
+  { strictNullChecks: true },
+]);
+
+testTypeHost('parses kitchen sink query (%o)', options => {
+  const virtualHost = ts.createVirtualHost({
+    ...ts.readVirtualModule('expect-type'),
+    ...ts.readVirtualModule('@0no-co/graphql.web'),
+    'kitchensinkQuery.ts': ts.readFileFromRoot('src/__tests__/fixtures/kitchensinkQuery.ts'),
+    'parser.ts': ts.readFileFromRoot('src/parser.ts'),
+    'index.ts': `
+      import { expectTypeOf } from 'expect-type';
+      import { kitchensinkQuery, kitchensinkDocument } from './kitchensinkQuery';
+      import type { Document } from './parser';
+
+      type actual = Document<typeof kitchensinkQuery>;
+
+      expectTypeOf<actual>().toEqualTypeOf<kitchensinkDocument>();
+    `,
+  });
+
+  ts.runDiagnostics(
+    ts.createTypeHost({
+      ...options,
+      rootNames: ['index.ts'],
+      host: virtualHost,
+    })
+  );
+});
