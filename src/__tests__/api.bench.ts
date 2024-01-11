f8c548007a88d7490165aa0cc1ff6c2c3907b540
Phil Pluckthun <phil@kitten.sh>
2024-01-11 14:31:32 +0000

    Add small bench file for src/api

---
+++ src/__tests__/api.bench.ts
@@ -0,0 +1,45 @@
+import { describe, bench } from 'vitest';
+import * as ts from './tsHarness';
+
+describe('Document', () => {
+  const virtualHost = ts.createVirtualHost({
+    ...ts.readVirtualModule('@0no-co/graphql.web'),
+    ...ts.readSourceFolders(['.']),
+    'simpleIntrospection.ts': ts.readFileFromRoot('src/__tests__/fixtures/simpleIntrospection.ts'),
+    'index.ts': `
+      import { simpleIntrospection } from './simpleIntrospection';
+      import { ResultOf, initGraphQLTada, readFragment } from './api';
+
+      const graphql = initGraphQLTada<{ introspection: typeof simpleIntrospection; }>();
+
+      const todoFragment = graphql(\`
+        fragment TodoData on Todo {
+          id
+          text
+        }
+      \`);
+
+      const todoQuery = graphql(\`
+        query {
+          todos {
+            id
+            complete
+            ...TodoData
+          }
+        }
+      \`, [todoFragment]);
+
+      const result: ResultOf<typeof todoQuery> = {} as any;
+      const todo = readFragment(todoFragment, result?.todos?.[0]);
+    `,
+  });
+
+  const typeHost = ts.createTypeHost({
+    rootNames: ['index.ts'],
+    host: virtualHost,
+  });
+
+  bench('create simple query', () => {
+    ts.runDiagnostics(typeHost);
+  });
+});
