a158ae9f24dcb5877f4957e4635f07f95f564fa9
Phil Pluckthun <phil@kitten.sh>
2024-01-11 13:00:30 +0000

    Add tests for src/namespace

---
+++ src/__tests__/namespace.test-d.ts
@@ -0,0 +1,76 @@
+import { describe, it, expectTypeOf } from 'vitest';
+import type { Kind } from '@0no-co/graphql.web';
+
+import type { $tada, decorateFragmentDef, getFragmentsOfDocumentsRec } from '../namespace';
+
+describe('decorateFragmentDef', () => {
+  it('creates an annotated fragment definition', () => {
+    type input = {
+      kind: Kind.DOCUMENT;
+      definitions: [
+        {
+          kind: Kind.FRAGMENT_DEFINITION;
+          name: {
+            kind: Kind.NAME;
+            value: 'TodoFragment';
+          };
+          typeCondition: {
+            kind: Kind.NAMED_TYPE;
+            name: {
+              kind: Kind.NAME;
+              value: 'Todo';
+            };
+          };
+          directives: unknown;
+          selectionSet: unknown;
+        },
+      ];
+    };
+
+    type actual = decorateFragmentDef<input>;
+    type expected = {
+      kind: Kind.FRAGMENT_DEFINITION;
+      name: {
+        kind: Kind.NAME;
+        value: 'TodoFragment';
+      };
+      typeCondition: {
+        kind: Kind.NAMED_TYPE;
+        name: {
+          kind: Kind.NAME;
+          value: 'Todo';
+        };
+      };
+      readonly [$tada.fragmentId]: symbol;
+    };
+
+    expectTypeOf<actual>().toMatchTypeOf<expected>();
+  });
+});
+
+describe('getFragmentsOfDocumentsRec', () => {
+  type inputFragmentDef = {
+    kind: Kind.FRAGMENT_DEFINITION;
+    name: {
+      kind: Kind.NAME;
+      value: 'TodoFragment';
+    };
+    typeCondition: {
+      kind: Kind.NAMED_TYPE;
+      name: {
+        kind: Kind.NAME;
+        value: 'Todo';
+      };
+    };
+    readonly [$tada.fragmentId]: unique symbol;
+  };
+
+  type input = {
+    [$tada.fragmentDef]?: inputFragmentDef;
+  };
+
+  type actual = getFragmentsOfDocumentsRec<[input]>;
+  type expected = { TodoFragment: inputFragmentDef };
+
+  expectTypeOf<actual>().toMatchTypeOf<expected>();
+});
