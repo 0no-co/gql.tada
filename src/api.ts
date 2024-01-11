bd4ce71bd27a61c5b26d8bd47aae6752966bf6f4
Phil Pluckthun <phil@kitten.sh>
2024-01-11 12:25:58 +0000

    Add TSDocs for public API

--- src/api.ts
+++ src/api.ts
@@ -23,11 +23,38 @@ import type { getVariablesType } from './variables';
 import type { parseDocument, DocumentNodeLike } from './parser';
 import type { stringLiteral, matchOr, DocumentDecoration } from './utils';
 
+/** Private interface used to constrain [setupSchema]. */
 interface AbstractSetupSchema {
   introspection: IntrospectionQuery;
   scalars: ScalarsLike;
 }
 
+/** This is used to configure gql.tada with your introspection data and scalars.
+ *
+ * @remarks
+ * You must extend this interface via declaration merging with your {@link IntrospectionQuery}
+ * data and optionally your scalars to get proper type inference.
+ * This is done by declaring a declaration for it as per the following example.
+ *
+ * Configuring scalars is optional and by default the standard scalrs are already
+ * defined.
+ *
+ * @example
+ *
+ * ```
+ * import { myIntrospection } from './myIntrospection';
+ *
+ * declare module 'gql.tada' {
+ *   interface setupSchema {
+ *     introspection: typeof myIntrospection;
+ *     scalars: {
+ *       DateTime: string;
+ *       Json: any;
+ *     };
+ *   }
+ * }
+ * ```
+ */
 interface setupSchema extends AbstractSetupSchema {
   /*empty*/
 }
@@ -37,6 +64,16 @@ type Schema = mapIntrospection<
   matchOr<ScalarsLike, setupSchema['scalars'], {}>
 >;
 
+/** Alias to a GraphQL parse function returning an exact document type.
+ *
+ * @param input - A string of a GraphQL document
+ * @returns A parsed {@link DocumentNode}.
+ *
+ * @remarks
+ * This function accepts a GraphQL document string and parses it, just like
+ * GraphQL’s `parse` function. However, its return type will be the exact
+ * structure of the AST parsed in types.
+ */
 function parse<const In extends stringLiteral<In>>(input: In): parseDocument<In> {
   return _parse(input) as any;
 }
@@ -55,6 +92,46 @@ type getDocumentNode<
       >
   : never;
 
+/** Function to create and compose GraphQL documents with result and variable types.
+ *
+ * @param input - A string of a GraphQL document.
+ * @param fragments - An optional list of other GraphQL fragments created with this function.
+ * @returns A {@link DocumentNode} with result and variables types.
+ *
+ * @remarks
+ * This function creates a {@link DocumentNode} with result and variables types.
+ * It is used with your schema in {@link setupSchema} to create a result type
+ * of your queries, fragments, and variables.
+ *
+ * You can compose fragments into this function by passing them and a fragment
+ * mask will be created for them.
+ * When creating queries, the returned document of queries can be passed into GraphQL clients
+ * which will then automatically infer the result and variables types.
+ *
+ * @example
+ *
+ * ```
+ * import { graphql } from 'gql.tada';
+ *
+ * const bookFragment = graphql(`
+ *   fragment BookComponent on Book {
+ *     id
+ *     title
+ *   }
+ * `);
+ *
+ * const bookQuery = graphql(`
+ *   query Book ($id: ID!) {
+ *     book(id: $id) {
+ *       id
+ *       ...BookComponent
+ *     }
+ *   }
+ * `, [bookFragment]);
+ * ```
+ *
+ * @see {@link readFragment} for how to read from fragment masks.
+ */
 function graphql<
   const In extends stringLiteral<In>,
   const Fragments extends readonly [...DocumentDefDecorationLike[]],
@@ -75,22 +152,17 @@ function graphql<
   return { kind: Kind.DOCUMENT, definitions: [...definitions] } as any;
 }
 
-/** A GraphQL `DocumentNode` with attached generics for its result data and variables.
+/** A GraphQL `DocumentNode` with attached types for results and variables.
  *
  * @remarks
- * A GraphQL {@link DocumentNode} defines both the variables it accepts on request and the `data`
- * shape it delivers on a response in the GraphQL query language.
+ * This is a GraphQL {@link DocumentNode} with attached types for results and variables.
+ * This is used by GraphQL clients to infer the types of results and variables and provide
+ * type-safety in GraphQL documents.
  *
- * To bridge the gap to TypeScript, tools may be used to generate TypeScript types that define the shape
- * of `data` and `variables` ahead of time. These types are then attached to GraphQL documents using this
- * `TypedDocumentNode` type.
+ * You can create typed GraphQL documents using the {@link graphql} function.
  *
- * Using a `DocumentNode` that is typed like this will cause any `urql` API to type its input `variables`
- * and resulting `data` using the types provided.
- *
- * @privateRemarks
- * For compatibility reasons this type has been copied and internalized from:
- * https://github.com/dotansimha/graphql-typed-document-node/blob/3711b12/packages/core/src/index.ts#L3-L10
+ * `Result` is the type of GraphQL results, as returned by GraphQL APIs for a given query.
+ * `Variables` is the type of variables, as accepted by GraphQL APIs for a given query.
  *
  * @see {@link https://github.com/dotansimha/graphql-typed-document-node} for more information.
  */
@@ -102,14 +174,58 @@ interface TadaDocumentNode<
     DocumentDecoration<Result, Variables>,
     makeFragmentDefDecoration<Decoration> {}
 
+/** A utility type returning the `Result` type of typed GraphQL documents.
+ *
+ * @remarks
+ * This accepts a {@link TadaDocumentNode} and returns the attached `Result` type
+ * of GraphQL documents.
+ */
 type ResultOf<Document> = Document extends DocumentDecoration<infer Result, infer _>
   ? Result
   : never;
 
+/** A utility type returning the `Variables` type of typed GraphQL documents.
+ *
+ * @remarks
+ * This accepts a {@link TadaDocumentNode} and returns the attached `Variables` type
+ * of GraphQL documents.
+ */
 type VariablesOf<Document> = Document extends DocumentDecoration<infer _, infer Variables>
   ? Variables
   : never;
 
+/** Creates a fragment mask for a given fragment document.
+ *
+ * @remarks
+ * When {@link graphql} is used to create a fragment and is spread into another
+ * fragment or query, their result types will only contain a “reference” to the
+ * fragment. This encourages isolation and is known as “fragment masking.”
+ *
+ * While {@link readFragment} is used to unmask these fragment masks, this utility
+ * creates a fragment mask, so you can accept the masked data in the part of your
+ * codebase that defines a fragment.
+ *
+ * @example
+ *
+ * ```
+ * import { FragmentOf, graphql, readFragment } from 'gql.tada';
+ *
+ * const bookFragment = graphql(`
+ *   fragment BookComponent on Book {
+ *     id
+ *     title
+ *   }
+ * `);
+ *
+ * // May be called with any GraphQL data that contains a spread of `bookFragment`
+ * const getBook = (data: FragmentOf<typeof bookFragment>) => {
+ *   // Unmasks the fragment and casts to the result type of `bookFragment`
+ *   const book = readFragment(bookFragment, data);
+ * };
+ * ```
+ *
+ * @see {@link readFragment} for how to read from fragment masks.
+ */
 type FragmentOf<Document extends DocumentDefDecorationLike> = Exclude<
   Document[$tada.fragmentDef],
   undefined
@@ -131,6 +247,55 @@ type fragmentOfTypeRec<Document extends DocumentDefDecorationLike> =
   | undefined
   | null;
 
+/** Unmasks a fragment mask for a given fragment document and data.
+ *
+ * @param _document - A GraphQL document of a fragment, created using {@link graphql}.
+ * @param fragment - A mask of the fragment, which can be wrapped in arrays, or nullable.
+ * @returns The unmasked data of the fragment.
+ *
+ * @remarks
+ * When {@link graphql} is used to create a fragment and is spread into another
+ * fragment or query, their result types will only contain a “reference” to the
+ * fragment. This encourages isolation and is known as “fragment masking.”
+ *
+ * This means that you must use {@link readFragment} to unmask these fragment masks
+ * and get to the data. This encourages isolation and only using the data you define
+ * a part of your codebase to require.
+ *
+ * @example
+ *
+ * ```
+ * import { FragmentOf, graphql, readFragment } from 'gql.tada';
+ *
+ * const bookFragment = graphql(`
+ *   fragment BookComponent on Book {
+ *     id
+ *     title
+ *   }
+ * `);
+ *
+ * const getBook = (data: FragmentOf<typeof bookFragment> | null) => {
+ *   // Unmasks the fragment and casts to the result type of `bookFragment`
+ *   // This is intersected with `| null` in this case, due to the input type.
+ *   const book = readFragment(bookFragment, data);
+ * };
+ *
+ * const bookQuery = graphql(`
+ *   query Book ($id: ID!) {
+ *     book(id: $id) {
+ *       id
+ *       ...BookComponent
+ *     }
+ *   }
+ * `, [bookFragment]);
+ *
+ * const getQuery = (data: ResultOf<typeof bookQuery>) => {
+ *   getBook(data?.book);
+ * };
+ * ```
+ *
+ * @see {@link readFragment} for how to read from fragment masks.
+ */
 function readFragment<
   const Document extends DocumentDefDecorationLike,
   const Fragment extends fragmentOfTypeRec<Document>,
