474a301922b33f09ccda397a715602bc0dfaa25c
Phil Pluckthun <phil@kitten.sh>
2024-03-20 10:03:10 +0000

    Add cleaned up implementation for TS virtual FS Host

---
+++ packages/cli-utils/src/vfs/host.ts
@@ -0,0 +1,67 @@
+import type { ScriptTarget, CreateSourceFileOptions, SourceFile } from 'typescript';
+
+import { createSourceFile } from 'typescript';
+import { posix as path } from 'node:path';
+
+import type { TargetCache } from './utils';
+import { createTargetCache, setTargetCache, getTargetCache } from './utils';
+
+export type FileData = Uint8Array | string;
+export type Files = Record<string, FileData>;
+
+export class File {
+  cache: TargetCache<SourceFile>;
+  name: string;
+  data: Uint8Array | null;
+  text: string | null;
+  constructor(name: string, data: Uint8Array | string) {
+    this.cache = createTargetCache();
+    this.name = name;
+    if (typeof data === 'string') {
+      this.text = data;
+      this.data = null;
+    } else {
+      this.text = null;
+      this.data = data;
+    }
+  }
+
+  toSourceFile(opts: ScriptTarget | CreateSourceFileOptions) {
+    const target = typeof opts === 'object' ? opts.languageVersion : opts;
+    return (
+      getTargetCache(this.cache, target) ||
+      setTargetCache(this.cache, target, createSourceFile(this.name, this.toString(), opts))
+    );
+  }
+
+  toBuffer(): Uint8Array {
+    return this.data || (this.data = new TextEncoder().encode(this.text!));
+  }
+
+  toString() {
+    return this.text || (this.text = new TextDecoder().decode(this.data!));
+  }
+}
+
+export class Directory {
+  children: Record<string, Directory | undefined>;
+  files: Record<string, File | undefined>;
+  constructor() {
+    this.children = Object.create(null);
+    this.files = Object.create(null);
+  }
+
+  getOrCreateDirectory(name: string): Directory {
+    return this.children[name] || (this.children[name] = new Directory());
+  }
+}
+
+export function normalize(filename: string) {
+  return path.normalize(!filename.startsWith(path.sep) ? path.sep + filename : filename);
+}
+
+export function split(filename: string): string[] {
+  return filename !== path.sep ? filename.split(path.sep).slice(1) : [];
+}
+
+export const sep = path.sep;
---
+++ packages/cli-utils/src/vfs/index.ts
@@ -0,0 +1,116 @@
+import type {
+  CompilerHost,
+  ResolvedModule,
+  CreateSourceFileOptions,
+  ScriptTarget,
+} from 'typescript';
+
+import { createModuleResolutionCache, resolveModuleName } from 'typescript';
+
+import { compilerOptions } from './compilerOptions';
+import { Directory, File, split, normalize, sep } from './host';
+
+const ROOT_LIB_DTS_PATH = 'lib.d.ts';
+const ROOT_LIB_DTS_DATA = '';
+
+export type VirtualCompilerHost = ReturnType<typeof createVirtualHost> & CompilerHost;
+
+export function createVirtualHost() {
+  const cache = createModuleResolutionCache(sep, normalize, compilerOptions);
+
+  const root = new Directory();
+  root.files[ROOT_LIB_DTS_PATH] = new File(ROOT_LIB_DTS_PATH, ROOT_LIB_DTS_DATA);
+
+  return {
+    getCanonicalFileName: normalize,
+
+    getDefaultLibFileName() {
+      return sep + ROOT_LIB_DTS_PATH;
+    },
+    getCurrentDirectory() {
+      return sep;
+    },
+    getNewLine() {
+      return '\n';
+    },
+    getModuleResolutionCache() {
+      return cache;
+    },
+    useCaseSensitiveFileNames() {
+      return true;
+    },
+
+    fileExists(filename: string) {
+      const parts = split(normalize(filename));
+      let directory: Directory | undefined = root;
+      for (let i = 0; i < parts.length - 1; i++) {
+        directory = directory.children[parts[i]];
+        if (!directory) return false;
+      }
+      return !!directory.files[parts[parts.length - 1]];
+    },
+
+    directoryExists(directoryName: string) {
+      const parts = split(normalize(directoryName));
+      let directory: Directory | undefined = root;
+      for (let i = 0; i < parts.length - 1; i++) {
+        directory = directory.children[parts[i]];
+        if (!directory) return false;
+      }
+      return !!directory.children[parts[parts.length - 1]];
+    },
+
+    writeFile(filename: string, content: Uint8Array | string) {
+      const name = normalize(filename);
+      const parts = split(name);
+      let directory = root;
+      for (let i = 0; i < parts.length - 1; i++)
+        directory = directory.getOrCreateDirectory(parts[i]);
+      directory.files[parts[parts.length - 1]] = new File(name, content);
+    },
+
+    getDirectories(directoryName: string) {
+      const parts = split(normalize(directoryName));
+      let directory: Directory | undefined = root;
+      for (let i = 0; i < parts.length; i++) {
+        directory = directory.children[parts[i]];
+        if (!directory) return [];
+      }
+      return Object.keys(directory.children);
+    },
+
+    readFile(filename: string) {
+      const parts = split(normalize(filename));
+      let directory: Directory | undefined = root;
+      for (let i = 0; i < parts.length - 1; i++) {
+        directory = directory.children[parts[i]];
+        if (!directory) return undefined;
+      }
+      const file = directory.files[parts[parts.length - 1]];
+      return file && file.toString();
+    },
+
+    getSourceFile(
+      filename: string,
+      languageVersionOrOptions: ScriptTarget | CreateSourceFileOptions
+    ) {
+      const parts = split(normalize(filename));
+      let directory: Directory | undefined = root;
+      for (let i = 0; i < parts.length - 1; i++) {
+        directory = directory.children[parts[i]];
+        if (!directory) return undefined;
+      }
+      const file = directory.files[parts[parts.length - 1]];
+      return file && file.toSourceFile(languageVersionOrOptions);
+    },
+
+    resolveModuleNames(moduleNames: string[], containingFile: string) {
+      const resolvedModules: (ResolvedModule | undefined)[] = [];
+      for (const moduleName of moduleNames) {
+        const result = resolveModuleName(moduleName, containingFile, compilerOptions, this, cache);
+        resolvedModules.push(result.resolvedModule);
+      }
+      return resolvedModules;
+    },
+  } satisfies CompilerHost;
+}
