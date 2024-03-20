ed047ab4d4f427c5ac1765fb5e6b738d6ab7766d
Phil Pluckthun <phil@kitten.sh>
2024-03-20 16:53:57 +0000

    Use @gql.tada/internal tools in CLI

--- packages/cli-utils/src/tada.ts
+++ packages/cli-utils/src/tada.ts
@@ -1,28 +1,15 @@
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
 import type { GraphQLSchema, IntrospectionQuery } from 'graphql';
+
 import {
   buildClientSchema,
   buildSchema,
   getIntrospectionQuery,
   introspectionFromSchema,
 } from 'graphql';
-import { minifyIntrospectionQuery } from '@urql/introspection';
 
-import { introspectionToSchemaType } from './introspection';
-
-export const tadaGqlContents = `import { initGraphQLTada } from 'gql.tada';
-import type { introspection } from './introspection';
-
-export const graphql = initGraphQLTada<{
-  introspection: typeof introspection;
-}>();
-
-export type { FragmentOf, ResultOf, VariablesOf } from 'gql.tada';
-export type { FragmentOf as FragmentType } from 'gql.tada';
-export { readFragment } from 'gql.tada';
-export { readFragment as useFragment } from 'gql.tada';
-`;
+import { minifyIntrospection, outputIntrospectionFile } from '@gql.tada/internal';
 
 /**
  * This function mimics the behavior of the LSP, this so we can ensure
@@ -35,61 +22,35 @@ export async function ensureTadaIntrospection(
   schemaLocation: SchemaOrigin,
   outputLocation: string,
   base: string = process.cwd(),
-  shouldPreprocess = false
+  shouldPreprocess = true
 ) {
   const writeTada = async () => {
-    try {
-      const schema = await loadSchema(base, schemaLocation);
-      if (!schema) {
-        console.error('Something went wrong while trying to load the schema.');
-        return;
-      }
-      const introspection = introspectionFromSchema(schema, {
-        descriptions: false,
-      });
-      const minified = minifyIntrospectionQuery(introspection, {
-        includeDirectives: false,
-        includeEnums: true,
-        includeInputs: true,
-        includeScalars: true,
-      });
-
-      const json = JSON.stringify(minified, null, 2);
-      const resolvedOutputLocation = path.resolve(base, outputLocation);
-      let contents;
-
-      if (/\.d\.ts$/.test(outputLocation)) {
-        const outputType = shouldPreprocess ? await introspectionToSchemaType(json) : json;
-        contents = [
-          preambleComments,
-          dtsAnnotationComment,
-          `export type introspection = ${outputType};\n`,
-          "import * as gqlTada from 'gql.tada';\n",
-          "declare module 'gql.tada' {",
-          '  interface setupSchema {',
-          '    introspection: introspection',
-          '  }',
-          '}',
-        ].join('\n');
-      } else if (path.extname(outputLocation) === '.ts') {
-        contents = [
-          preambleComments,
-          tsAnnotationComment,
-          `const introspection = ${json} as const;\n`,
-          'export { introspection };',
-        ].join('\n');
-      } else {
-        console.warn('Invalid file extension for tadaOutputLocation.');
-        return;
-      }
-
-      await fs.writeFile(resolvedOutputLocation, contents);
-    } catch (e) {
-      console.error('Something went wrong while writing the introspection file', e);
+    const schema = await loadSchema(base, schemaLocation);
+    if (!schema) {
+      console.error('Something went wrong while trying to load the schema.');
+      return;
     }
+
+    const introspection = minifyIntrospection(
+      introspectionFromSchema(schema, {
+        descriptions: false,
+      })
+    );
+
+    const contents = await outputIntrospectionFile(introspection, {
+      fileType: outputLocation,
+      shouldPreprocess,
+    });
+
+    const resolvedOutputLocation = path.resolve(base, outputLocation);
+    await fs.writeFile(resolvedOutputLocation, contents);
   };
 
-  await writeTada();
+  try {
+    await writeTada();
+  } catch (error) {
+    console.error('Something went wrong while writing the introspection file', error);
+  }
 }
 
 export type SchemaOrigin =
@@ -167,40 +128,3 @@ export const loadSchema = async (
       : schemaOrIntrospection;
   }
 };
-
-const preambleComments = ['/* eslint-disable */', '/* prettier-ignore */'].join('\n') + '\n';
-
-const dtsAnnotationComment = [
-  '/** An IntrospectionQuery representation of your schema.',
-  ' *',
-  ' * @remarks',
-  ' * This is an introspection of your schema saved as a file by GraphQLSP.',
-  ' * It will automatically be used by `gql.tada` to infer the types of your GraphQL documents.',
-  ' * If you need to reuse this data or update your `scalars`, update `tadaOutputLocation` to',
-  ' * instead save to a .ts instead of a .d.ts file.',
-  ' */',
-].join('\n');
-
-const tsAnnotationComment = [
-  '/** An IntrospectionQuery representation of your schema.',
-  ' *',
-  ' * @remarks',
-  ' * This is an introspection of your schema saved as a file by GraphQLSP.',
-  ' * You may import it to create a `graphql()` tag function with `gql.tada`',
-  ' * by importing it and passing it to `initGraphQLTada<>()`.',
-  ' *',
-  ' * @example',
-  ' * ```',
-  " * import { initGraphQLTada } from 'gql.tada';",
-  " * import type { introspection } from './introspection';",
-  ' *',
-  ' * export const graphql = initGraphQLTada<{',
-  ' *   introspection: typeof introspection;',
-  ' *   scalars: {',
-  ' *     DateTime: string;',
-  ' *     Json: any;',
-  ' *   };',
-  ' * }>();',
-  ' * ```',
-  ' */',
-].join('\n');
