c4210d09eb48f5186b90bf247555680671cb6382
Phil Pluckthun <phil@kitten.sh>
2024-03-21 00:54:13 +0000

    Refactor introspection output to not use TS

--- packages/internal/src/introspection.ts
+++ packages/internal/src/introspection.ts
@@ -1,33 +1,14 @@
-import type { IntrospectionQuery } from 'graphql';
+import type { IntrospectionQuery, IntrospectionType } from 'graphql';
 
-import { TypeFormatFlags } from 'typescript';
 import { minifyIntrospectionQuery } from '@urql/introspection';
 
 import { PREAMBLE_IGNORE, ANNOTATION_DTS, ANNOTATION_TS } from './constants';
-import { TSError, TadaError } from './errors';
-
-import {
-  createVirtualHost,
-  importLib,
-  importModule,
-  resolveModuleFile,
-  createProgram,
-} from './vfs';
-
-const ROOT_FILE = 'index.ts';
-const ALIAS_PREFIX = '$__';
-
-const BUILDER_FLAGS =
-  TypeFormatFlags.NoTruncation |
-  TypeFormatFlags.NoTypeReduction |
-  TypeFormatFlags.InTypeAlias |
-  TypeFormatFlags.UseFullyQualifiedType |
-  TypeFormatFlags.GenerateNamesForShadowedTypeParams;
+import { TadaError } from './errors';
 
 const stringifyJson = (input: unknown | string): string =>
   typeof input === 'string' ? input : JSON.stringify(input, null, 2);
 const stringifyName = (input: string | undefined | null): string =>
-  input ? JSON.stringify(input) : 'never';
+  input ? `"${input}"` : 'never';
 
 export function minifyIntrospection(introspection: IntrospectionQuery): IntrospectionQuery {
   return minifyIntrospectionQuery(introspection, {
@@ -38,14 +19,70 @@ export function minifyIntrospection(introspection: IntrospectionQuery): Introspe
   });
 }
 
-async function createHost(evaluateFile: string) {
-  const host = createVirtualHost();
-  await importLib(host);
-  await importModule(host, '@0no-co/graphql.web');
-  host.writeFile('gql-tada.ts', await resolveModuleFile('gql.tada/dist/gql-tada.d.ts'));
-  host.writeFile(ROOT_FILE, evaluateFile);
-  return host;
-}
+const stringifyObjectType = (object: Record<string, string>) => {
+  let output = '';
+  for (const key in object) output += `${stringifyName(key)}: ${object[key]}; `;
+  return `{ ${output}}`;
+};
+
+const stringifyTupleType = (tuple: Array<string | Record<string, string>>) => {
+  let output = '';
+  for (const value of tuple)
+    output += `${typeof value === 'string' ? value : stringifyObjectType(value)}}, `;
+  return `[ ${output}]`;
+};
+
+export const printIntrospectionType = (type: IntrospectionType) => {
+  if (type.kind === 'ENUM') {
+    return stringifyObjectType({
+      kind: stringifyName(type.kind),
+      name: stringifyName(type.name),
+      type: type.enumValues.map((value) => stringifyName(value.name)).join(' | '),
+    });
+  } else if (type.kind === 'INPUT_OBJECT') {
+    return stringifyObjectType({
+      kind: stringifyName(type.kind),
+      name: stringifyName(type.name),
+      inputFields: stringifyTupleType(
+        type.inputFields.map((inputField) => JSON.stringify(inputField))
+      ),
+    });
+  } else if (type.kind === 'OBJECT') {
+    return stringifyObjectType({
+      kind: stringifyName(type.kind),
+      name: stringifyName(type.name),
+      fields: stringifyObjectType(
+        type.fields.reduce((object, field) => {
+          object[field.name] = JSON.stringify(field);
+          return object;
+        }, {})
+      ),
+    });
+  } else if (type.kind === 'INTERFACE') {
+    return stringifyObjectType({
+      kind: stringifyName(type.kind),
+      name: stringifyName(type.name),
+      possibleTypes: type.possibleTypes.map((value) => stringifyName(value.name)).join(' | '),
+      fields: stringifyObjectType(
+        type.fields.reduce((object, field) => {
+          object[field.name] = JSON.stringify(field);
+          return object;
+        }, {})
+      ),
+    });
+  } else if (type.kind === 'UNION') {
+    return stringifyObjectType({
+      kind: stringifyName(type.kind),
+      name: stringifyName(type.name),
+      possibleTypes: type.possibleTypes.map((value) => stringifyName(value.name)).join(' | '),
+      fields: '{}',
+    });
+  } else if (type.kind === 'SCALAR') {
+    return 'unknown';
+  } else {
+    return 'never';
+  }
+};
 
 export async function preprocessIntrospection({
   __schema: schema,
@@ -54,35 +91,10 @@ export async function preprocessIntrospection({
   const mutationName = stringifyName(schema.mutationType && schema.mutationType.name);
   const subscriptionName = stringifyName(schema.subscriptionType && schema.subscriptionType.name);
 
-  // TODO: We'd love for this to be just evaluated from `mapIntrospection` instead
-  // However, currently TypeScript seems to have hard limits in both the printer and checker for how
-  // many types are evaluated inside large object types.
-  // A fix/hack to get TypeScript to output the entire type would be preferable to splitting printing
-  // up here.
-  let evaluateFile = 'import type { __mapType } from "./gql-tada.ts";\n';
-  for (const type of schema.types) {
-    evaluateFile += `export type ${ALIAS_PREFIX + type.name} = __mapType<${JSON.stringify(
-      type
-    )}>;\n`;
-  }
-
-  const host = await createHost(evaluateFile);
-  const program = createProgram([ROOT_FILE], host);
-  const checker = program.getTypeChecker();
-  const diagnostics = program.getSemanticDiagnostics();
-  const root = program.getSourceFile(ROOT_FILE);
-  const rootSymbol = root && checker.getSymbolAtLocation(root);
-  if (diagnostics.length || !rootSymbol)
-    throw new TSError('TypeScript failed to evaluate introspection', diagnostics);
-
   let evaluatedTypes = '';
-  for (const exportSymbol of checker.getExportsOfModule(rootSymbol)) {
-    const declaration = exportSymbol && exportSymbol.declarations && exportSymbol.declarations[0];
-    const type = declaration && checker.getTypeAtLocation(declaration);
-    if (!type) throw new TSError('Something went wrong while evaluating introspection type.');
-    const typeStr = checker.typeToString(type, undefined, BUILDER_FLAGS).trim();
-    const nameStr = exportSymbol.name.slice(ALIAS_PREFIX.length);
-    evaluatedTypes += `    ${stringifyName(nameStr)}: ${typeStr};\n`;
+  for (const type of schema.types) {
+    const typeStr = printIntrospectionType(type);
+    evaluatedTypes += `    "${type.name}": ${typeStr};\n`;
   }
 
   return (
