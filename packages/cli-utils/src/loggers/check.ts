946e7f6c1625135b2e22dc4ce3f9df10dd307353
Phil Pluckthun <phil@kitten.sh>
2024-04-11 22:13:16 +0100

    Update check logger for new interface

--- packages/cli-utils/src/loggers/check.ts
+++ packages/cli-utils/src/loggers/check.ts
@@ -1,87 +1,151 @@
-import { CLIError, Chars, Icons, HeavyBox, Style, circleSpinner, init, cmd, log } from '../term';
+import {
+  pipe,
+  interval,
+  fromValue,
+  fromAsyncIterable,
+  switchMap,
+  concat,
+  delay,
+  map,
+  scan,
+} from 'wonka';
+
+import * as t from '../term';
 export { error } from '../term';
 
-function delay(ms = 750) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+const clearLine = () => t.cmd(t.CSI.DeleteLines, 1) + t.cmd(t.CSI.ToColumn, 1);
 
 function printTitle(title: string) {
-  log([
-    cmd(cmd.Style, Style.BrightBlack),
-    HeavyBox.TopLeft,
-    Chars.Space,
-    cmd(cmd.Style, [Style.Magenta, Style.Invert]),
-    Chars.Space,
-    title.trim(),
-    Chars.Space,
-    cmd(cmd.Style, Style.Reset),
-    Chars.Newline,
-  ]);
+  return fromValue(
+    t.text([
+      t.cmd(t.CSI.Style, t.Style.BrightBlack),
+      t.HeavyBox.TopLeft,
+      ' ',
+      t.cmd(t.CSI.Style, [t.Style.Magenta, t.Style.Invert]),
+      ` ${title.trim()} `,
+      t.cmd(t.CSI.Style, t.Style.Reset),
+      '\n',
+    ])
+  );
 }
 
 function printLine(text?: string) {
-  log([cmd(cmd.Style, Style.BrightBlack), HeavyBox.Vertical, Chars.Space]);
-  if (text) log(text);
-  log([cmd(cmd.Style, Style.Reset), Chars.Newline]);
+  let out = t.text([t.cmd(t.CSI.Style, t.Style.BrightBlack), t.HeavyBox.Vertical]);
+  if (text) out += t.text` ${text}\n${t.HeavyBox.Vertical}`;
+  out += t.text([t.cmd(t.CSI.Style, t.Style.Reset), '\n']);
+  return fromValue(out);
 }
 
 function printCompletedTask(description: string, isLast = false) {
-  log([
-    cmd(cmd.Style, Style.BrightBlack),
-    isLast ? HeavyBox.BottomLeft : HeavyBox.VerticalRight,
-    Chars.Space,
-    cmd(cmd.Style, Style.Green),
-    Icons.TickSwoosh,
-    Chars.Space,
-    cmd(cmd.Style, Style.Foreground),
-    description,
-    cmd(cmd.Style, Style.Reset),
-    Chars.Newline,
-  ]);
+  return fromValue(
+    t.text([
+      t.cmd(t.CSI.Style, t.Style.BrightBlack),
+      isLast ? t.HeavyBox.BottomLeft : t.HeavyBox.VerticalRight,
+      ' ',
+      t.cmd(t.CSI.Style, t.Style.Green),
+      t.Icons.TickSwoosh,
+      ' ',
+      t.cmd(t.CSI.Style, t.Style.Foreground),
+      description,
+      t.cmd(t.CSI.Style, t.Style.Reset),
+      '\n',
+    ])
+  );
 }
 
 function printFailedTask(description: string) {
-  log([
-    cmd(cmd.Style, Style.BrightBlack),
-    HeavyBox.BottomLeft,
-    Chars.Space,
-    cmd(cmd.Style, Style.BrightRed),
-    Icons.CrossSwoosh,
-    Chars.Space,
-    cmd(cmd.Style, Style.Foreground),
-    description,
-    cmd(cmd.Style, Style.Reset),
-    Chars.Newline,
-  ]);
+  return fromValue(
+    t.text([
+      t.cmd(t.CSI.Style, t.Style.BrightBlack),
+      t.HeavyBox.BottomLeft,
+      ' ',
+      t.cmd(t.CSI.Style, t.Style.BrightRed),
+      t.Icons.CrossSwoosh,
+      ' ',
+      t.cmd(t.CSI.Style, t.Style.Foreground),
+      description,
+      t.cmd(t.CSI.Style, t.Style.Reset),
+      '\n',
+    ])
+  );
 }
 
 function printRunningTask(description: string) {
-  let state = 0;
-  function print() {
-    if (state) {
-      log([cmd(cmd.DeleteLines, 1), cmd(cmd.ToColumn, 1)]);
-    }
-    log([
-      cmd(cmd.Style, Style.Magenta),
-      circleSpinner[state++ % circleSpinner.length],
-      Chars.Space,
-      cmd(cmd.Style, Style.Foreground),
-      description,
-      cmd(cmd.Style, Style.Reset),
-    ]);
-  }
-
-  const intervalId = setInterval(print, 150);
-  return () => {
-    clearInterval(intervalId);
-    log([cmd(cmd.DeleteLines, 1), cmd(cmd.ToColumn, 1)]);
-  };
+  return pipe(
+    interval(150),
+    map((state) =>
+      t.text([
+        clearLine(),
+        t.cmd(t.CSI.Style, t.Style.Magenta),
+        t.circleSpinner[state % t.circleSpinner.length],
+        ' ',
+        t.cmd(t.CSI.Style, t.Style.Foreground),
+        description,
+        t.cmd(t.CSI.Style, t.Style.Reset),
+      ])
+    )
+  );
 }
 
-interface PrintSignal {
-  text: string;
+function printSuccess() {
+  return fromValue(
+    t.text([
+      t.Chars.Newline,
+      t.cmd(t.CSI.Style, [t.Style.Green, t.Style.Invert]),
+      ' Done ',
+      t.cmd(t.CSI.Style, t.Style.NoInvert),
+      t.Chars.Space,
+      'You are all set and ready to go.',
+    ])
+  );
+}
+
+function printError(error: Error) {
+  if (error instanceof t.CLIError) {
+    return fromValue(
+      t.text([
+        t.Chars.Newline,
+        t.cmd(t.CSI.Style, [t.Style.BrightRed, t.Style.Invert]),
+        ' Error ',
+        t.cmd(t.CSI.Style, t.Style.NoInvert),
+        t.Chars.Newline,
+        `${error.output}`,
+      ])
+    );
+  } else {
+    return fromValue(
+      t.text([
+        t.Chars.Newline,
+        t.cmd(t.CSI.Style, [t.Style.BrightRed, t.Style.Invert]),
+        ' Unexpected Error ',
+        t.cmd(t.CSI.Style, t.Style.NoInvert),
+        t.Chars.Newline,
+        `${error}`,
+      ])
+    );
+  }
+}
+
+async function* printTask(task: () => AsyncIterable<PrintSignal>): AsyncIterable<PrintSignal> {
+  try {
+    for await (const signal of task()) {
+      yield signal;
+      if (signal.kind && signal.kind !== 'update') return;
+    }
+    yield { kind: 'complete' };
+  } catch (error: any) {
+    yield { kind: 'error', error };
+  }
+}
+
+type PrintSignal =
+  | { kind: 'complete' }
+  | { kind: 'error'; error: Error }
+  | { kind?: 'update'; text: string };
+
+interface PrintState {
+  prev: { kind?: 'update'; text: string } | null;
+  task: PrintSignal;
 }
 
 interface PrintConfig {
@@ -91,71 +155,34 @@ interface PrintConfig {
 }
 
 export async function print(config: PrintConfig) {
-  init();
+  const tty = t.initTTY();
 
-  printTitle(config.title);
-  if (config.description) printLine(config.description);
+  const print = concat([
+    printTitle(config.title),
+    printLine(config.description),
+    pipe(
+      fromAsyncIterable(printTask(config.task)),
+      delay(700),
+      scan((state: PrintState, task) => ({ task, prev: state.task as PrintState['prev'] }), {
+        prev: null,
+        task: null,
+      } as any),
+      switchMap(({ prev, task }) => {
+        const sources = [fromValue(clearLine())];
+        if (task.kind === 'complete') {
+          if (prev) sources.push(printCompletedTask(prev.text, true));
+          sources.push(printSuccess());
+        } else if (task.kind === 'error') {
+          if (prev) sources.push(printFailedTask(prev.text));
+          sources.push(printError(task.error));
+        } else {
+          if (prev) sources.push(printCompletedTask(prev.text));
+          sources.push(printRunningTask(task.text));
+        }
+        return concat(sources);
+      })
+    ),
+  ]);
 
-  printLine();
-
-  let current: PrintSignal | undefined;
-  let cancel: (() => void) | undefined;
-  try {
-    for await (const signal of config.task()) {
-      if (cancel) {
-        cancel();
-      }
-      if (current) {
-        printCompletedTask(current.text);
-        printLine();
-      }
-      cancel = printRunningTask((current = signal).text);
-      await delay();
-    }
-
-    if (cancel) {
-      cancel();
-    }
-    if (current) {
-      printCompletedTask(current.text, true);
-      printLine();
-    }
-
-    log([
-      Chars.Newline,
-      cmd(cmd.Style, [cmd.style.Green, cmd.style.Invert]),
-      ' Done ',
-      cmd(cmd.Style, cmd.style.NoInvert),
-      Chars.Space,
-      'You are all set and ready to go.',
-    ]);
-  } catch (error: any) {
-    if (cancel) {
-      cancel();
-    }
-
-    if (current) {
-      printFailedTask(current.text);
-    }
-
-    if (error instanceof CLIError) {
-      log([
-        Chars.Newline,
-        cmd(cmd.Style, [cmd.style.BrightRed, cmd.style.Invert]),
-        ' Error ',
-        cmd(cmd.Style, cmd.style.NoInvert),
-        Chars.Newline,
-        `${error.output}`,
-      ]);
-    } else {
-      log([
-        Chars.Newline,
-        cmd(cmd.Style, [cmd.style.BrightRed, cmd.style.Invert]),
-        ' Unexpected Error ',
-        cmd(cmd.Style, cmd.style.NoInvert),
-        Chars.Newline,
-        `${error}`,
-      ]);
-    }
-  }
+  return await tty.pipeOutput(print);
 }
