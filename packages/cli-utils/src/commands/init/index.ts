11c4a7471f287616d0bd99ee7e55c54452749f5b
Phil Pluckthun <phil@kitten.sh>
2024-04-13 00:36:02 +0100

    Move over init command

--- packages/cli-utils/src/commands/init/index.ts
+++ packages/cli-utils/src/commands/init/index.ts
@@ -1,9 +1,14 @@
-import { Command } from 'clipanion';
+import * as path from 'node:path';
+import { Command, Option } from 'clipanion';
+import { run } from './runner';
 
 export class InitCommand extends Command {
   static paths = [['init']];
 
+  input = Option.String({ name: 'dir' });
+
   async execute() {
-    return 0;
+    const target = path.resolve(process.cwd(), this.input);
+    await run(target);
   }
 }
