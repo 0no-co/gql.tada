312cdfbbba76f98900c862884dd79909700fc2dc
Phil Pluckthun <phil@kitten.sh>
2024-04-12 23:22:04 +0100

    Allow CLIErrors to flow through composed output

--- packages/cli-utils/src/term/write.ts
+++ packages/cli-utils/src/term/write.ts
@@ -23,9 +23,15 @@ export const stripAnsi = (input: string) => input.replace(ansiRegex, '');
 
 export class CLIError extends Error {
   output: string;
-  constructor(message: string) {
+  exit: number;
+  constructor(message: string, exitCode?: number) {
     super(stripAnsi(message));
     this.output = message;
+    this.exit = exitCode || 1;
+  }
+
+  toString() {
+    return this.output;
   }
 }
 
@@ -48,10 +54,13 @@ function text(input: string | readonly string[], ...args: readonly string[]): st
 }
 
 function error(input: readonly string[], ...args: readonly string[]): CLIError;
+function error(arg: string | number, ...input: readonly string[]): CLIError;
 function error(...input: readonly string[]): CLIError;
 
-function error(...input: any[]): CLIError {
-  return new CLIError(text(...input));
+function error(arg: readonly string[] | string | number, ...input: readonly string[]): CLIError {
+  return typeof arg === 'number'
+    ? new CLIError(text(...input), arg)
+    : new CLIError(text(arg as string, ...input));
 }
 
 function clear(text: string) {
@@ -64,38 +73,42 @@ function clear(text: string) {
   );
 }
 
-type ComposeInput = undefined | string | Source<string> | AsyncIterable<ComposeInput>;
+type ComposeInput = undefined | string | CLIError | Source<string> | AsyncIterable<ComposeInput>;
 
 async function* convertError(outputs: AsyncIterable<ComposeInput>): AsyncIterable<ComposeInput> {
   try {
     yield* outputs;
   } catch (error) {
-    yield error instanceof CLIError ? error.output : '' + error;
+    yield error instanceof CLIError ? error : '' + error;
   }
 }
 
-function compose(outputs: AsyncIterable<ComposeInput>): Source<string> {
+function compose(outputs: AsyncIterable<ComposeInput>): Source<string | CLIError> {
   const outputs$ = share(fromAsyncIterable(convertError(outputs)));
   const reset = cmd(CSI.Style, [Style.Reset, Style.NoInvert]);
   return pipe(
     outputs$,
     filter(<T>(x: T): x is Exclude<T, undefined> => x != null),
     concatMap((output) => {
-      if (typeof output === 'object') {
+      if (typeof output === 'object' && !(output instanceof CLIError)) {
         return compose(output);
       }
-      const output$ = share(typeof output === 'string' ? fromValue(output) : output);
+      const output$ = share(
+        typeof output === 'string' || output instanceof CLIError ? fromValue(output) : output
+      );
       return pipe(
         merge([
           pipe(
             output$,
             sample(outputs$),
-            map((output) => (output.endsWith('\n') ? output : '')),
+            map((output) => (typeof output === 'string' && output.endsWith('\n') ? output : '')),
             take(1)
           ),
           pipe(output$, takeUntil(outputs$)),
         ]),
-        scan((prev, output) => clear(prev) + output + reset, '')
+        scan((prev: CLIError | string, output) => {
+          return typeof output === 'string' ? clear('' + prev) + output + reset : output;
+        }, '')
       );
     })
   );
