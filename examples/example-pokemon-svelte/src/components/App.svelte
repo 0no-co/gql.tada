d3f227969be4388b3cfc2742c74e7ecf9dca0dab
Phil Pluckthun <phil@kitten.sh>
2024-04-21 23:24:26 +0100

    Add Svelte example

---
+++ examples/example-pokemon-svelte/src/components/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Client, setContextClient, cacheExchange, fetchExchange } from "@urql/svelte";
+  import PokemonList from "./PokemonList.svelte";
+
+  setContextClient(new Client({
+    url: "https://trygql.formidable.dev/graphql/basic-pokedex",
+    exchanges: [cacheExchange, fetchExchange],
+  }));
+</script>
+
+<PokemonList />
