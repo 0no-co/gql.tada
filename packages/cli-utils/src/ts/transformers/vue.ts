2a295b183f717f79330a14b7d82e1e44a7557604
Phil Pluckthun <phil@kitten.sh>
2024-04-24 18:00:48 +0100

    patch(cli): Upgrade `@vue/language-core` and maintain backwards-compatibility (#253)

--- packages/cli-utils/src/ts/transformers/vue.ts
+++ packages/cli-utils/src/ts/transformers/vue.ts
@@ -4,22 +4,27 @@ import { forEachEmbeddedCode, getDefaultVueLanguagePlugins } from '@vue/language
 import * as vueCompilerDOM from '@vue/compiler-dom';
 import * as vue from '@vue/language-core';
 
-const VueVirtualCode =
-  vue.VueGeneratedCode || ((vue as any).VirtualCode as typeof vue.VueGeneratedCode);
+let VueVirtualCode: typeof vue.VueVirtualCode | undefined;
+if ('VueVirtualCode' in vue) {
+  VueVirtualCode = vue.VueVirtualCode;
+} else if ('VueGeneratedCode' in vue) {
+  VueVirtualCode = (vue as any).VueGeneratedCode;
+}
 
 const vueCompilerOptions = vue.resolveVueCompilerOptions({});
 
 let plugins: ReturnType<typeof getDefaultVueLanguagePlugins> | undefined;
 
 export const transform = (sourceFile: ts.SourceFile): VirtualCode | undefined => {
-  if (!plugins) {
+  if (!VueVirtualCode) {
+    return undefined;
+  } else if (!plugins) {
     plugins = getDefaultVueLanguagePlugins({
       modules: {
         typescript: ts,
         '@vue/compiler-dom': vueCompilerDOM,
       },
       compilerOptions: {},
-      codegenStack: false,
       globalTypesHolder: undefined,
       vueCompilerOptions,
     });
@@ -32,8 +37,7 @@ export const transform = (sourceFile: ts.SourceFile): VirtualCode | undefined =>
     snapshot,
     vueCompilerOptions,
     plugins,
-    ts,
-    false
+    ...([ts, false] as any as [typeof ts])
   );
   for (const code of forEachEmbeddedCode(root)) if (code.id.startsWith('script_')) return code;
 };
