31a572cc5b4fa94d02adf7d8245cfc7cc3234f7b
Phil Pluckthun <phil@kitten.sh>
2024-04-26 14:27:10 +0100

    Remove redundant default object

--- packages/cli-utils/src/ts/factory.ts
+++ packages/cli-utils/src/ts/factory.ts
@@ -55,7 +55,6 @@ export const programFactory = (params: ProgramFactoryParams): ProgramFactory => 
   const options = {
     ...ts.getDefaultCompilerOptions(),
     getDefaultLibFilePath: ts.getDefaultLibFilePath(config.options),
-    ...defaultCompilerOptions,
     ...config.options,
   };
   const host = createVirtualCompilerHost(system, options, ts);
@@ -77,7 +76,7 @@ export const programFactory = (params: ProgramFactoryParams): ProgramFactory => 
         typeof params.sourceText === 'object'
           ? params.sourceText.getText(0, params.sourceText.getLength())
           : params.sourceText,
-        options.target,
+        options.target || ts.ScriptTarget.ESNext,
         /*setParentNodes*/ true,
         scriptKind || (params.scriptKind != null ? params.scriptKind : ts.ScriptKind.TSX)
       );
@@ -173,10 +172,6 @@ export const programFactory = (params: ProgramFactoryParams): ProgramFactory => 
   return factory;
 };
 
-const defaultCompilerOptions = {
-  target: ts.ScriptTarget.Latest,
-} satisfies ts.CompilerOptions;
-
 const resolveDefaultLibsPath = (params: ProgramFactoryParams): string => {
   const target = ts.getDefaultLibFilePath({});
   if (!ts.sys.fileExists(target)) {
@@ -209,7 +204,7 @@ const resolveConfig = (params: ProgramFactoryParams, system: ts.System): ts.Pars
     parseResult.config,
     system,
     projectRoot,
-    defaultCompilerOptions,
+    ts.getDefaultCompilerOptions(),
     params.configPath
   );
 };
